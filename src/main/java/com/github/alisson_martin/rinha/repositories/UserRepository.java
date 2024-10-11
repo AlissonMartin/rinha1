203082b523887b4e7af5182496fd5239a437b52b
Alisson Martin <alissonmartin2011@hotmail.com>
2024-10-11 00:25:09 -0300

    Improve: Best configuration so far

--- src/main/java/com/github/alisson_martin/rinha/repositories/UserRepository.java
+++ src/main/java/com/github/alisson_martin/rinha/repositories/UserRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
-  @Query(value = "SELECT * FROM users WHERE concatsearch ILIKE %:search%", nativeQuery = true)
+  @Query(value = "SELECT * FROM users WHERE concatsearch ILIKE %:search% LIMIT 50", nativeQuery = true)
   List<User> findUsers(@Param("search") String search);
 }
